b5f64049fcc6308aad12b06e17297ad5ebbd8607
Justin <justinbraun1999@web.de>
2022-01-17 20:07:05 +0100

    BACKEND-1 Login with Public/Private key * Testing Certification Authentication

--- src/App/AppModule.ts
+++ src/App/AppModule.ts
@@ -4,6 +4,13 @@ import {TestResolver} from "./resolver/TestResolver";
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import configuration from "../../config/configuration";
 import {AuthModule} from "../Auth/AuthModule";
+import {readFileSync, writeFile, writeFileSync} from "fs";
+import * as crypto from "crypto";
+import {JsonWebKeyInput} from "crypto";
+import {isKeyObject} from "util/types";
+import {addPackageJsonDependency} from "@nestjs/schematics/dist/utils/dependencies.utils";
+import * as fs from "fs";
+var jwkToPem = require('jwk-to-pem')
 
 @Module({
     imports: [
@@ -21,4 +28,10 @@ import {AuthModule} from "../Auth/AuthModule";
     controllers: [],
     providers: [TestResolver],
 })
-export class AppModule {}
+export class AppModule {
+    constructor() {
+        //console.log(crypto.publicEncrypt(buffer.toString(), Buffer.from('Hello there :>')).toString('base64'))
+        //console.log(crypto.publicEncrypt(jwkToPem(jwk), Buffer.from('aaa')).toString('base64'));
+    }
+
+}
--- src/Auth/TestController.ts
+++ src/Auth/TestController.ts
@@ -1,6 +1,7 @@
-﻿import {Controller, Get, Post, Query} from '@nestjs/common';
+﻿import {Body, Controller, Get, Header, Param, Post, Query, UploadedFile, UseInterceptors} from '@nestjs/common';
 import {AuthService} from "./services/AuthService";
 import * as crypto from "crypto";
+import {FileInterceptor} from "@nestjs/platform-express";
 
 @Controller()
 export class TestController {
@@ -11,8 +12,27 @@ export class TestController {
     public testa() {
         return this.authService.signJwt('0x00')
     }
-    @Post('authtest')
-    public authtest(@Query('publicKey') publicKey, @Query('cipher') cipher) {
-        return this.authService.decryptCipher(publicKey, cipher)
+
+    @Post('encrypt')
+    public encrypt(@Body('cert') cert, @Body('message') message) {
+        return crypto.publicEncrypt(this.convertDerPublicToPem(cert), Buffer.from(message, 'utf-8')).toString('base64');
+    }
+
+    @Post('decrypt')
+    public decrypt(@Body('cert') cert, @Body('message') message) {
+        console.log(this.convertDerPrivateToPem(cert));
+        return crypto.privateDecrypt(this.convertDerPrivateToPem(cert), Buffer.from(message, 'base64')).toString();
+    }
+
+    convertDerPublicToPem(der: string) {
+        var prefix = '-----BEGIN RSA PUBLIC KEY-----\n';
+        var postfix = '-----END RSA PUBLIC KEY-----';
+        return prefix + der.match(/.{0,64}/g).join('\n') + postfix;
+    }
+
+    convertDerPrivateToPem(der: string) {
+        var prefix = '-----BEGIN RSA PRIVATE KEY-----\n';
+        var postfix = '-----END RSA PRIVATE KEY-----';
+        return prefix + der.match(/.{0,64}/g).join('\n') + postfix;
     }
 }
