098adf6aeab07b5e31292e0cc0aba49cb97b39ad
Justin <justinbraun1999@web.de>
2022-01-31 01:04:25 +0100

    BACKEND-1 Login with Public/Private key * Added TypeORM to App Module

--- src/App/AppModule.ts
+++ src/App/AppModule.ts
@@ -1,16 +1,10 @@
 import {Module} from "@nestjs/common";
-import {GraphQLModule} from "@nestjs/graphql";
 import {TestResolver} from "./resolver/TestResolver";
-import {ConfigModule, ConfigService} from "@nestjs/config";
+import {ConfigModule} from "@nestjs/config";
 import configuration from "../../config/configuration";
 import {AuthModule} from "../Auth/AuthModule";
-import {readFileSync, writeFile, writeFileSync} from "fs";
-import * as crypto from "crypto";
-import {JsonWebKeyInput} from "crypto";
-import {isKeyObject} from "util/types";
-import {addPackageJsonDependency} from "@nestjs/schematics/dist/utils/dependencies.utils";
-import * as fs from "fs";
-var jwkToPem = require('jwk-to-pem')
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {DaemonModule} from "../Daemon/DaemonModule";
 
 @Module({
     imports: [
@@ -18,20 +12,18 @@ var jwkToPem = require('jwk-to-pem')
             load: [configuration],
             isGlobal: true
         }),
-        // TypeOrmModule.forRoot(),
-        GraphQLModule.forRoot({
-            autoSchemaFile: true,
-            //context: ({req}) => ({currentUser: req.user}),
+        // @ts-ignore
+        TypeOrmModule.forRoot({
+            autoLoadEntities: true,
+            entities: [
+                "dist/src/**/Entities/*.js",
+            ],
+            ...configuration().database,
         }),
-        AuthModule
+        AuthModule,
+        DaemonModule
     ],
     controllers: [],
     providers: [TestResolver],
 })
-export class AppModule {
-    constructor() {
-        //console.log(crypto.publicEncrypt(buffer.toString(), Buffer.from('Hello there :>')).toString('base64'))
-        //console.log(crypto.publicEncrypt(jwkToPem(jwk), Buffer.from('aaa')).toString('base64'));
-    }
-
-}
+export class AppModule {}
