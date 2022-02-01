57d125113cef110c2951cf8d11acd2c8cf4e5d41
Justin <justinbraun1999@web.de>
2022-02-01 22:41:51 +0100

    BACKEND-1 Login with Public/Private key * Moved from default logger to TSLOG using LoggerBridge

---
+++ src/LoggerBridge.ts
@@ -0,0 +1,53 @@
+﻿import { LoggerService } from '@nestjs/common';
+import {Logger} from "tslog";
+import {yellow} from "colors";
+
+export class LoggerBridge implements LoggerService {
+    private logger: Logger;
+    constructor() {
+        this.logger = new Logger({
+            displayFunctionName: false,
+            displayFilePath: 'hidden',
+            displayLoggerName: false
+        });
+    }
+
+    /**
+     * Write a 'log' level log.
+     */
+    log(message: any, ...optionalParams: any[]) {
+        this.logger.info(this.transformParams(...optionalParams), message)
+    }
+
+    /**
+     * Write an 'error' level log.
+     */
+    error(message: any, ...optionalParams: any[]) {
+        this.logger.error(this.transformParams(...optionalParams), message)
+    }
+
+    /**
+     * Write a 'warn' level log.
+     */
+    warn(message: any, ...optionalParams: any[]) {
+        this.logger.warn(this.transformParams(...optionalParams), message)
+    }
+
+    /**
+     * Write a 'debug' level log.
+     */
+    debug?(message: any, ...optionalParams: any[]) {
+        this.logger.debug(this.transformParams(...optionalParams), message)
+    }
+
+    /**
+     * Write a 'verbose' level log.
+     */
+    verbose?(message: any, ...optionalParams: any[]) {
+        this.logger.trace(this.transformParams(...optionalParams), message)
+    }
+
+    private transformParams(...optionalParams: any[]): string {
+        return `[${yellow(optionalParams.join(', '))}]`;
+    };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './App/AppModule';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './App/AppModule';
+import * as fs from "fs";
+import {LoggerBridge} from "./LoggerBridge";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,  {
+    httpsOptions: {
+      key: fs.readFileSync('certs/server_key.pem'),
+      cert: fs.readFileSync('certs/server_cert.pem'),
+      ca: fs.readFileSync('certs/server_cert.pem'),
+      requestCert: true,
+      rejectUnauthorized: false
+    },
+    logger: new LoggerBridge()
+  });
   await app.listen(3000);
 }
 bootstrap();
